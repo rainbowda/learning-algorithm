b317635fe426a65b5d140f321d81cb3810e67d69
huangzd <930211680@qq.com>
2018-10-20 11:29:29 +0800

    自底向上

--- src/main/java/com/learningAlgorithm/dynamicProgramming/DynamicPramming3.java
+++ src/main/java/com/learningAlgorithm/dynamicProgramming/DynamicPramming3.java
@@ -4,4 +4,31 @@ package com.learningAlgorithm.dynamicProgramming;
  * 第三版自底向上
  */
 public class DynamicPramming3 {
+
+    public static int bottomUpCutRod(int[] worth, int length){
+        int[] r = new int[length+1];
+
+        int value = 0;
+
+        for (int j = 1; j <= length; j++) {
+
+            for(int i = 1; i <= j; i++){
+                int nextValue = worth[i] + r[j-i];
+                value = value > nextValue ? value : nextValue;
+            }
+            r[j] = value;
+        }
+
+        return r[length];
+    }
+
+    public static void main(String[] args) {
+        int[] worth = {0, 1, 5, 8, 9, 10, 17, 17, 20, 24, 30};
+
+        int length = 10;
+
+        int value = bottomUpCutRod(worth, length);
+
+        System.out.println(value);
+    }
 }
