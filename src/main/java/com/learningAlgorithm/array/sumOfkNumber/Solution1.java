32736938d9f24343934b74d1d123b89d5896dcf0
huangzd <930211680@qq.com>
2018-10-25 08:30:59 +0800

    寻找和为定值的多个数

---
+++ src/main/java/com/learningAlgorithm/array/sumOfkNumber/Solution1.java
@@ -0,0 +1,9 @@
+package com.learningAlgorithm.array.sumOfkNumber;
+
+/**
+ * n问题转换为n-1问题
+ */
+public class Solution1 {
+
+
+}
---
+++ src/main/java/com/learningAlgorithm/array/sumOfkNumber/Solution2.java
@@ -0,0 +1,9 @@
+package com.learningAlgorithm.array.sumOfkNumber;
+
+/**
+ * 回溯剪枝
+ */
+public class Solution2 {
+
+
+}
