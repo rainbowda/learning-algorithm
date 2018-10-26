1aea1cbede15ca337d7b1d2ca733b237030c1e6b
huangzd <930211680@qq.com>
2018-10-26 22:58:34 +0800

    矩阵相乘

--- src/main/java/com/learningAlgorithm/array/mulMatrix/Solution1.java
+++ src/main/java/com/learningAlgorithm/array/mulMatrix/Solution1.java
@@ -5,11 +5,11 @@ package com.learningAlgorithm.array.mulMatrix;
  */
 public class Solution1 {
 
-    void mulMatrix(int[][] matrixA, int[][] matrixB, int[][] matrixC) {
-        for (int i = 0; i < 2; ++i) {
-            for (int j = 0; j < 2; ++j) {
+    void mulMatrix(int[][] matrixA, int[][] matrixB, int[][] matrixC, int n) {
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
                 matrixC[i][j] = 0;
-                for (int k = 0; k < 2; ++k) {
+                for (int k = 0; k < n; ++k) {
                     matrixC[i][j] += matrixA[i][k] * matrixB[k][j];
                 }
             }
---
+++ src/main/java/com/learningAlgorithm/array/mulMatrix/Solution2.java
@@ -0,0 +1,11 @@
+package com.learningAlgorithm.array.mulMatrix;
+
+/**
+ * 施特拉森算法
+ */
+public class Solution2 {
+
+
+
+
+}
