351144d1229ae15711f5e759788a9d12dfd84a94
huangzd <930211680@qq.com>
2018-10-27 10:28:04 +0800

    字符串的查找 蛮力匹配方法

--- src/main/java/com/learningAlgorithm/query/stringSearch/Solution1.java
+++ src/main/java/com/learningAlgorithm/query/stringSearch/Solution1.java
@@ -6,9 +6,26 @@ package com.learningAlgorithm.query.stringSearch;
 public class Solution1 {
 
     int violentMatch(char[] s, char[] p){
+        int sLen = s.length;
+        int pLen = p.length;
 
+        int i = 0,j = 0;
 
-        return 0;
+        while (i < sLen && j < pLen){
+            if (s[i] == p[j]){
+                i++;
+                j++;
+            } else {
+                i = i-j+1;
+                j = 0;
+            }
+        }
+
+        if (j == pLen){
+            return i - j;
+        } else {
+            return -1;
+        }
     }
 
 }
