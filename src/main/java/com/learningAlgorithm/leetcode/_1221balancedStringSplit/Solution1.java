93f759ce0c3a2bc27ea12243d3503cb141c1370e
rainbowda <930211680@qq.com>
2021-09-18 22:01:23 +0800

    1221

--- src/main/java/com/learningAlgorithm/leetcode/_1221balancedStringSplit/Solution1.java
+++ src/main/java/com/learningAlgorithm/leetcode/_1221balancedStringSplit/Solution1.java
@@ -5,7 +5,19 @@ import java.util.Arrays;
 class Solution1 {
     public int balancedstringsplit(String s) {
 
+        int ans = 0, d = 0;
+        for (int i = 0; i < s.length(); ++i) {
+            char ch = s.charAt(i);
+            if (ch == 'L') {
+                ++d;
+            } else {
+                --d;
+            }
+            if (d == 0) {
+                ++ans;
+            }
+        }
+        return ans;
 
-        return 0;
     }
 }
