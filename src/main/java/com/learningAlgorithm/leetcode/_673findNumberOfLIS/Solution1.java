c1d6a43dde1d8565b1b9f2e9be28391253b04746
rainbowda <930211680@qq.com>
2021-09-20 20:53:12 +0800

    solution

--- src/main/java/com/learningAlgorithm/leetcode/_673findNumberOfLIS/Solution1.java
+++ src/main/java/com/learningAlgorithm/leetcode/_673findNumberOfLIS/Solution1.java
@@ -1,5 +1,30 @@
 package com.learningAlgorithm.leetcode._673findNumberOfLIS;
 
 class Solution1 {
-
+    public int findNumberOfLIS(int[] nums) {
+        int n = nums.length, maxLen = 0, ans = 0;
+        int[] dp = new int[n];
+        int[] cnt = new int[n];
+        for (int i = 0; i < n; ++i) {
+            dp[i] = 1;
+            cnt[i] = 1;
+            for (int j = 0; j < i; ++j) {
+                if (nums[i] > nums[j]) {
+                    if (dp[j] + 1 > dp[i]) {
+                        dp[i] = dp[j] + 1;
+                        cnt[i] = cnt[j]; // 重置计数
+                    } else if (dp[j] + 1 == dp[i]) {
+                        cnt[i] += cnt[j];
+                    }
+                }
+            }
+            if (dp[i] > maxLen) {
+                maxLen = dp[i];
+                ans = cnt[i]; // 重置计数
+            } else if (dp[i] == maxLen) {
+                ans += cnt[i];
+            }
+        }
+        return ans;
+    }
 }
