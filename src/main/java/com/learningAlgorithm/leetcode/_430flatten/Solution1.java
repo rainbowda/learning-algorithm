3e46291e84b2670af404646dda84ecfdba8db6e2
rainbowda <930211680@qq.com>
2021-09-24 20:41:28 +0800

    430. 扁平化多级双向链表

--- src/main/java/com/learningAlgorithm/leetcode/_430flatten/Solution1.java
+++ src/main/java/com/learningAlgorithm/leetcode/_430flatten/Solution1.java
@@ -1,5 +1,49 @@
 package com.learningAlgorithm.leetcode._430flatten;
 
 class Solution1 {
+    class Node {
+        public int val;
+        public Node prev;
+        public Node next;
+        public Node child;
+    };
+
+    public Node flatten(Node head) {
+        dfs(head);
+        return head;
+    }
+
+    public Node dfs(Node node) {
+        Node cur = node;
+        // 记录链表的最后一个节点
+        Node last = null;
+
+        while (cur != null) {
+            Node next = cur.next;
+            //  如果有子节点，那么首先处理子节点
+            if (cur.child != null) {
+                Node childLast = dfs(cur.child);
+
+                next = cur.next;
+                //  将 node 与 child 相连
+                cur.next = cur.child;
+                cur.child.prev = cur;
+
+                //  如果 next 不为空，就将 last 与 next 相连
+                if (next != null) {
+                    childLast.next = next;
+                    next.prev = childLast;
+                }
+
+                // 将 child 置为空
+                cur.child = null;
+                last = childLast;
+            } else {
+                last = cur;
+            }
+            cur = next;
+        }
+        return last;
+    }
 
 }
