1ec37a6b414480f5bbcce4d8f7d230325a7f2981
rainbowda <930211680@qq.com>
2021-09-25 10:08:43 +0800

    两个字符串的删除操作 解决方案1

--- src/main/java/com/learningAlgorithm/leetcode/_583minDistance/Solution1.java
+++ src/main/java/com/learningAlgorithm/leetcode/_583minDistance/Solution1.java
@@ -1,5 +1,21 @@
 package com.learningAlgorithm.leetcode._583minDistance;
 
 class Solution1 {
-
+    public int minDistance(String word1, String word2) {
+        int m = word1.length(), n = word2.length();
+        int[][] dp = new int[m + 1][n + 1];
+        for (int i = 1; i <= m; i++) {
+            char c1 = word1.charAt(i - 1);
+            for (int j = 1; j <= n; j++) {
+                char c2 = word2.charAt(j - 1);
+                if (c1 == c2) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        int lcs = dp[m][n];
+        return m - lcs + n - lcs;
+    }
 }
