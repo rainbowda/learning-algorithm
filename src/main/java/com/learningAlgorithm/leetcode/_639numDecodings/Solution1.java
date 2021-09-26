366ff1fb4b309ca5c0921e7d7c9e0e2d8627d667
rainbowda <930211680@qq.com>
2021-09-27 07:32:32 +0800

    解码方法

---
+++ src/main/java/com/learningAlgorithm/leetcode/_639numDecodings/Solution1.java
@@ -0,0 +1,48 @@
+package com.learningAlgorithm.leetcode._639numDecodings;
+
+
+class Solution1 {
+    static final int MOD = 1000000007;
+
+    public int numDecodings(String s) {
+        int n = s.length();
+        // a = f[i-2], b = f[i-1], c = f[i]
+        long a = 0, b = 1, c = 0;
+        for (int i = 1; i <= n; ++i) {
+            c = b * check1digit(s.charAt(i - 1)) % MOD;
+            if (i > 1) {
+                c = (c + a * check2digits(s.charAt(i - 2), s.charAt(i - 1))) % MOD;
+            }
+            a = b;
+            b = c;
+        }
+        return (int) c;
+    }
+
+    public int check1digit(char ch) {
+        if (ch == '0') {
+            return 0;
+        }
+        return ch == '*' ? 9 : 1;
+    }
+
+    public int check2digits(char c0, char c1) {
+        if (c0 == '*' && c1 == '*') {
+            return 15;
+        }
+        if (c0 == '*') {
+            return c1 <= '6' ? 2 : 1;
+        }
+        if (c1 == '*') {
+            if (c0 == '1') {
+                return 9;
+            }
+            if (c0 == '2') {
+                return 6;
+            }
+            return 0;
+        }
+        return (c0 != '0' && (c0 - '0') * 10 + (c1 - '0') <= 26) ? 1 : 0;
+    }
+
+}
