09054e36c348484fc802194b002708743945596c
rainbowda <930211680@qq.com>
2021-09-29 07:28:34 +0800

    超级洗衣机

---
+++ src/main/java/com/learningAlgorithm/leetcode/_517findMinMoves/Solution1.java
@@ -0,0 +1,26 @@
+package com.learningAlgorithm.leetcode._517findMinMoves;
+
+import java.util.Arrays;
+
+/**
+ *
+ */
+class Solution1 {
+    public int findMinMoves(int[] machines) {
+        int tot = Arrays.stream(machines).sum();
+        int n = machines.length;
+        if (tot % n != 0) {
+            return -1;
+        }
+        int avg = tot / n;
+        int ans = 0, sum = 0;
+        for (int num : machines) {
+            num -= avg;
+            sum += num;
+            ans = Math.max(ans, Math.max(Math.abs(sum), num));
+        }
+        return ans;
+    }
+
+
+}
