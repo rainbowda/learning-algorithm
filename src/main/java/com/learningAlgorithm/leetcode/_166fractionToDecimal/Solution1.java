7ac2c767303129b8063b67c971a1521d64ee4b0c
rainbowda <930211680@qq.com>
2021-10-03 09:41:01 +0800

    1

---
+++ src/main/java/com/learningAlgorithm/leetcode/_166fractionToDecimal/Solution1.java
@@ -0,0 +1,49 @@
+package com.learningAlgorithm.leetcode._166fractionToDecimal;
+
+import java.util.*;
+
+/**
+ *
+ */
+class Solution1 {
+    public String fractionToDecimal(int numerator, int denominator) {
+        long numeratorLong = (long) numerator;
+        long denominatorLong = (long) denominator;
+        if (numeratorLong % denominatorLong == 0) {
+            return String.valueOf(numeratorLong / denominatorLong);
+        }
+
+        StringBuffer sb = new StringBuffer();
+        if (numeratorLong < 0 ^ denominatorLong < 0) {
+            sb.append('-');
+        }
+
+        // 整数部分
+        numeratorLong = Math.abs(numeratorLong);
+        denominatorLong = Math.abs(denominatorLong);
+        long integerPart = numeratorLong / denominatorLong;
+        sb.append(integerPart);
+        sb.append('.');
+
+        // 小数部分
+        StringBuffer fractionPart = new StringBuffer();
+        Map<Long, Integer> remainderIndexMap = new HashMap<Long, Integer>();
+        long remainder = numeratorLong % denominatorLong;
+        int index = 0;
+        while (remainder != 0 && !remainderIndexMap.containsKey(remainder)) {
+            remainderIndexMap.put(remainder, index);
+            remainder *= 10;
+            fractionPart.append(remainder / denominatorLong);
+            remainder %= denominatorLong;
+            index++;
+        }
+        if (remainder != 0) { // 有循环节
+            int insertIndex = remainderIndexMap.get(remainder);
+            fractionPart.insert(insertIndex, '(');
+            fractionPart.append(')');
+        }
+        sb.append(fractionPart.toString());
+
+        return sb.toString();
+    }
+}
