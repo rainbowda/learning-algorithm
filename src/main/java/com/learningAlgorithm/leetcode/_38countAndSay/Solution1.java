cc6ec33ca83e6462ddabf7377fb96681f3f2b456
rainbowda <930211680@qq.com>
2021-10-15 07:26:56 +0800

    外观数列

---
+++ src/main/java/com/learningAlgorithm/leetcode/_38countAndSay/Solution1.java
@@ -0,0 +1,30 @@
+package com.learningAlgorithm.leetcode._38countAndSay;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ *
+ */
+class Solution1 {
+    public String countAndSay(int n) {
+        String str = "1";
+        for (int i = 2; i <= n; ++i) {
+            StringBuilder sb = new StringBuilder();
+            int start = 0;
+            int pos = 0;
+
+            while (pos < str.length()) {
+                while (pos < str.length() && str.charAt(pos) == str.charAt(start)) {
+                    pos++;
+                }
+                sb.append(Integer.toString(pos - start)).append(str.charAt(start));
+                start = pos;
+            }
+            str = sb.toString();
+        }
+
+        return str;
+    }
+
+}
